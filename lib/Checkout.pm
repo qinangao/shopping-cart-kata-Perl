e81b1e243cd3593fd0dc35a35e073c9e9375e260
qinangao <qinan.gao@spareroom.co.uk>
2026-01-15 11:53:05 +0000

    Refactor: seperate validation logic from business logic

--- lib/Checkout.pm
+++ lib/Checkout.pm
@@ -29,6 +29,7 @@ my $PRODUCTS = {
 };
 
 # sub _load_products {
+
 #     my $file = path($Bin)->parent->child('data', 'pricingData.json');
 #     my $data = decode_json($file->slurp_utf8);
 
@@ -41,38 +42,70 @@ my $PRODUCTS = {
 
 # my $PRODUCTS = _load_products();
 
+# Validation:
+sub _validate_items {
+    my ($items) = @_;
+
+    unless (defined $items) {
+        die "Items field is required";
+    }
+
+    unless (ref $items eq 'ARRAY') {
+        die "Items must be an array";
+    }
+
+}
+
+sub _validate_item {
+    my ($item, $seen) = @_;
+
+    # Check for Invalid items
+    unless (defined $item && ref $item eq 'HASH') {
+        die "Invalid item";
+    }
+
+    my $code     = $item->{code};
+    my $quantity = $item->{quantity};
+
+    # Check for missing fields
+    unless (defined $code) {
+        die "Code is required";
+    }
+    unless (defined $quantity) {
+        die "Quantity is required";
+    }
+
+    unless ($quantity >= 0 && $quantity <= 1000) {
+        die "Quantity must be between 0 and 1000";
+    }
+
+    # Check for duplicate items
+    if ($seen->{$code}) {
+        die "Duplicate item $code is not allowed\n";
+    }
+    $seen->{$code} = 1;
+
+    # Check for invalid item code
+    unless ($PRODUCTS->{$code}) {
+        die "Product $code doesn't exist\n";
+    }
+}
+
+# Business logic:
 sub calculate_subtotal {
     my ($items) = @_;
+
+    _validate_items($items);
+
     my $subtotal = 0;
     my %seen;
 
     for my $item (@$items) {
-        # Check for Invalid items
-        unless (defined $item && ref $item eq 'HASH') {
-            die "Invalid item";
-        }
+
+        _validate_item($item, \%seen);
 
         my $code     = $item->{code};
         my $quantity = $item->{quantity};
-
-        # Check for missing fields
-        unless (defined $code) {
-            die "Code is required";
-        }
-        unless (defined $quantity) {
-            die "Quantity is required";
-        }
-
-        # Check for duplicate items
-        if ($seen{$code}) {
-            die "Duplicate item code $code not allowed\n";
-        }
-        $seen{$code} = 1;
-
-        # Check for invalid item code
-        unless ($PRODUCTS->{$code}) {
-            die "Product $code doesn't exist\n";
-        }
 
         my $unit_price = $PRODUCTS->{$code}{unit_price};
 
